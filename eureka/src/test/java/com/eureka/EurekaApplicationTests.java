e4d58e4efdecaa0055d8ae1aed7207ef873a654f
Raghad Aljuaid <rgaljuaid@saudia.com>
2023-02-12 21:10:05 +0300

    adding pricing service in eureka

---
+++ eureka/src/test/java/com/eureka/EurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
